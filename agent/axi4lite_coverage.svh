5fe4af92dec8d359159d2d16837dd5a238a3e5e0
Caneser Kenç <caneser_kenc@hotmail.com>
2025-12-24 18:06:12 +0300

    Refactor axi4lite_coverage class for improved readability and consistency

--- agent/axi4lite_coverage.svh
+++ agent/axi4lite_coverage.svh
@@ -1,24 +1,39 @@
 class axi4lite_coverage extends uvm_subscriber #(axi4lite_seq_item);
-
-    `uvm_component_utils(axi4lite_coverage)
-
-    axi4lite_seq_item coverage_item;
-
-    function new(string name,uvm_component parent);
-      super.new(name parent);
-      cg_axi = new();
-    endfunction 
-
-    covergroup cg_axi;
-
-    endgroup
+  
+  `uvm_component_utils(axi4lite_coverage)
 
 
+  axi4lite_seq_item cov_item;
 
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+    // Covergroupu oluştur 
+    cg_axi = new();
+  endfunction
 
-    virtual function void build_phase(uvm_phase phase);
-      super.build_phase(phase);
+  covergroup cg_axi;
+ 
+    cp_addr: coverpoint cov_item.addr {
+      bins low_range  = {[0:16]};           
+      bins mid_range  = {[17:100]};         
+      bins high_range = {[32'hFFFF_FFF0:$]};
+      bins others     = default; 
+    }
 
-    endfunction
+    cp_op: coverpoint cov_item.is_write {
+      bins write = {1}; 
+      bins read  = {0}; 
+    }
+
+    //CROSS COVERAGE 
+
+    cross_ops: cross cp_addr, cp_op;
+
+  endgroup
+
+  function void write(axi4lite_seq_item t);
+    cov_item = t;
+    cg_axi.sample();
+  endfunction
 
 endclass
